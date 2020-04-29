75a27c10cc845278f0b7eea7bb916619e551416a
Antonio Sanchez <antonio.sanchez.trespalacios@gmail.com>
2020-04-29 09:35:36 +0900

    minor detail fixed

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,12 +1,12 @@
-import Typed from 'typed.js';
+// import Typed from 'typed.js';
 
-const loadDynamicBannerText = () => {
-  new Typed('#banner-typed-text', {
-    strings: ["Change your life Pray for love, luck or even smite your enemies!"],
-    typeSpeed: 50,
-    loop: true
-  });
-}
+// const loadDynamicBannerText = () => {
+//   new Typed('#banner-typed-text', {
+//     strings: ["Change your life Pray for love, luck or even smite your enemies!"],
+//     typeSpeed: 50,
+//     loop: true
+//   });
+// }
 
-export { loadDynamicBannerText };
+// export { loadDynamicBannerText };
 
