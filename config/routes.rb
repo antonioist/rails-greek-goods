25841c75fbc960b15288b449d243e592a7183b07
Antonio Sanchez <antonio.sanchez.trespalacios@gmail.com>
2020-04-30 16:58:41 +0900

    home page created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'prayers#index'
+  root to: 'pages#home'
   resources :prayers, except: [:edit, :update] do
     resources :comments, only: [:new, :create]
   end
